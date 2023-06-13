e8610ab170e26ad123cee42edc0aad1e19373e8f
fuyunlong <1273179133@qq.com>
2023-06-13 18:55:15 +0800

    feat: 暂时提交

--- Toy-Browser/http/parser.js
+++ Toy-Browser/http/parser.js
@@ -1,4 +1,10 @@
-const EOF = Symbol("EOF") // end of line
+const EOF = Symbol("EOF") // end of line 结束状态
+let currentToken = null // 存储当前的token 
+
+/** 收集构造token  */
+function emit(token) {
+	console.log(token);
+}
 
 
 // <!DOCTYPE html> <html lang="en">
@@ -6,9 +12,16 @@ const EOF = Symbol("EOF") // end of line
 function data(c) {
     if(c==='<'){
         return tagOpen
-    }else if(c===EOF){
+		} else if (c === EOF) {
+			emit({
+				type:"EOF"
+			})
         return
-    }else{
+		} else {
+			emit({
+				type: 'text',
+				content:c
+			})
         return data // 文本节点
     }
 }
@@ -18,6 +31,10 @@ function tagOpen(c) {
 	if (c === '/') {
 		return endTagOpen
 	} else if (c.match(/^[a-zA-Z]$/)) {
+		currentToken = {
+			type: 'startTag',
+			tagName:''
+		}
 		return tagName(c)
 	} else {
 		return 
@@ -26,6 +43,10 @@ function tagOpen(c) {
 
 function endTagOpen(c) {
 	if (c.match(/^[a-zA-Z]$/)) {
+		currentToken = {
+			tyep: 'endTag',
+			tagName:''
+		}
 		return tagName(c)
 	} else if (c === '>') {
 		// 报错
@@ -35,7 +56,7 @@ function endTagOpen(c) {
 		
 	}
 }
-
+ 
 
 function tagName(c) {
 	// <html name=1 属性
@@ -44,26 +65,110 @@ function tagName(c) {
 	} else if (c === '/') {
 		return selfClosingStartTag
 	} else if (c.match(/^[a-zA-Z]$/)) {
+		currentToken.tagName += c
 		return tagName
 	} else if (c === '>') { 
+		emit(currentToken)
 		return data // 普通开始标签
 	} else {
 		return tagName
 	}
 }
 
-
+/** 处理属性 <html lang="en"> */
 function beforeAttributeName(c) {
 		if (c.match(/^[\t\n\f ]$/)) {
 			return beforeAttributeName
-		} else if (c === '>') {
-			return data
+		} else if (c === '>'||c==='/'||c===EOF) {
+			return afterAttributeName(c)
 		} else if (c === '=') {
-			return beforeAttributeName
+		
 		} else {
-			return beforeAttributeName
+			currentAttribute = {
+				name: '',
+				value:''
+			}
+			return attributeName(c)
 		}
 }
+
+function attributeName(c) {
+	if (c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
+		return afterAttributeName(c)
+	} else if (c === '=') {
+		return beforeAttributeValue
+	} else if (c === '\u0000') {
+		
+	} else if (c === "\"" || c === "'" || c === "<") {
+		
+	} else {
+		currentAttribute.name += c
+		return attributeName
+	}
+}
+
+function beforeAttributeValue(c) {
+	if (c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
+		return beforeAttributeValue
+	} else if (c === '\"') {
+		return doubleQuotedAttributeValue
+	} else if (c === '\'') {
+		return singleQuotedAttributeValue
+	} else if (c === '>') {
+		// return data
+	} else {
+				return UnQuotedAttributeValue(c)
+	}
+
+}
+
+function doubleQuotedAttributeValue(c) {
+	if (c === "\"") {
+		currentToken[currentAttribute.name] = currentAttribute.value
+		return afterQuotedAttributeValue
+	} else if (c === '\u0000') {
+		
+	} else if (c === EOF) {
+		
+	} else {
+		currentAttribute.value += c
+		return doubleQuotedAttributeValue
+	}
+}
+
+function singleQuotedAttributeValue(c) {
+	if (c === "'") {
+		currentToken[currentAttribute.name] = currentAttribute.value
+		return afterQuotedAttributeValue
+	} else if (c === '\u0000') {
+		
+	} else if (c === EOF) {
+		
+	} else {
+		currentAttribute.value += c
+		return doubleQuotedAttributeValue
+	}
+}
+
+// todo 3:49   37
+function UnQuotedAttributeValue(c) {
+	if (c.match(/^[\t\n\f ]$/)) {
+		currentToken[currentAttribute.name] = currentAttribute.value
+		return beforeAttributeName
+	} else if (c === '/') {
+		
+		currentToken[currentAttribute.name] = currentAttribute.value
+		return selfClosingStartTag
+	} else if (c === '>') {
+				currentToken[currentAttribute.name] = currentAttribute.value
+		emit(currentToken)
+		return data
+	} else if (c === '\u0000') {
+		
+	}else if(c==="\""||c==="'"||c==="<")
+}
+
+
 
 function selfClosingStartTag(c) {
 	if (c === '>') {
@@ -85,7 +190,6 @@ module.exports.parseHTML = function parseHTML(html){
     let state =  data
     for (let c of html) {
         state = state(c)
-
     }
     state = state(EOF)
     console.log('html',html)
