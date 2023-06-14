0586173875d477e354bf9ccd545a2c22d124b61a
fuyunlong <1273179133@qq.com>
2023-06-14 13:07:55 +0800

    feat: DOM树构建完成

--- Toy-Browser/http/parser.js
+++ Toy-Browser/http/parser.js
@@ -1,73 +1,77 @@
-const EOF = Symbol("EOF") // end of line 结束状态
-let currentToken = null // 存储当前的token 
-let currentAttribute = null
-let currentTextNode = null
+const EOF = Symbol('EOF') // end of line 结束状态
+
+
+let currentToken = null // 当前的token
+let currentAttribute = null // 当前属性
+let currentTextNode = null // 文本节点
+
+
 /** 收集构造token token构建dom  */
-let stack = [{type:'document',children:[]}]
+let stack = [{ type: 'document', children: [] }]
+
 /** 思路是：使用栈，遇到开始标签入栈，遇到结束标签出栈，让元素放入stack的dom中 */
 function emit(token) {
-	let top = stack[stack.length-1] // 取出栈顶元素
-	if(token.type==='startTag'){
+	let top = stack[stack.length - 1] // 取出栈顶元素
+	// {type: "startTag",tagName: "html",lang: "en"}
+	if (token.type === 'startTag') {
 		let element = {
-			type:'element',
-			children:[],
-			attributes:[]
+			type: 'element',
+			children: [],
+			attributes: []
 		}
 		element.tagName = token.tagName
+		// 将type和tagName的属性去除，其余的为属性
 		for (let p in token) {
-			if(p!=="type"&&p!=="tagName"){
+			if (p !== 'type' && p !== 'tagName') {
 				element.attributes.push({
-					name:p,
-					value:token[p]
+					name: p,
+					value: token[p]
 				})
 			}
 		}
 		top.children.push(element)
-		element.parent = top
-		if(!token.isSelfClosing){
+		// element.parent = top
+		if (!token.isSelfClosing) {
 			stack.push(element)
 		}
 		currentTextNode = null
-	}else if(token.type==='endTag'){
-		if(top.tagName!==token.tagName){
-			throw new Error('Tag start end doesn\'t match')
-		}else {
+	} else if (token.type === 'endTag') {
+		if (top.tagName !== token.tagName) {
+			throw new Error("Tag start end doesn't match")
+		} else {
 			stack.pop()
 		}
 		currentTextNode = null
-	}else if(token.type==='text'){
-		if(currentTextNode===null){
+	} else if (token.type === 'text') {
+		if (currentTextNode === null) {
 			currentTextNode = {
-				type:'text',
-				content:''
+				type: 'text',
+				content: ''
 			}
 			top.children.push(currentTextNode)
 		}
 		currentTextNode.content += token.content
 	}
-	console.log(token);
+	console.log(token)
 }
-
 
 // <!DOCTYPE html> <html lang="en">
 /** 开始处理标签 */
 function data(c) {
-    if(c==='<'){
-        return tagOpen
-		} else if (c === EOF) {
-			emit({
-				type:"EOF"
-			})
-        return
-		} else {
-			emit({
-				type: 'text',
-				content:c
-			})
-        return data // 文本节点
-    }
+	if (c === '<') {
+		return tagOpen
+	} else if (c === EOF) {
+		emit({
+			type: 'EOF'
+		})
+	} else {
+		emit({
+			type: 'text',
+			content: c
+		})
+		return data // 文本节点
+	}
 }
-
 
 function tagOpen(c) {
 	if (c === '/') {
@@ -75,30 +79,29 @@ function tagOpen(c) {
 	} else if (c.match(/^[a-zA-Z]$/)) {
 		currentToken = {
 			type: 'startTag',
-			tagName:''
+			tagName: ''
 		}
 		return tagName(c)
 	} else {
-		return 
+		emit({
+			type: 'text',
+			content:c
+		})
 	}
 }
 
 function endTagOpen(c) {
 	if (c.match(/^[a-zA-Z]$/)) {
 		currentToken = {
-			tyep: 'endTag',
-			tagName:''
+			type: 'endTag',
+			tagName: ''
 		}
 		return tagName(c)
 	} else if (c === '>') {
-		// 报错
 	} else if (c === EOF) {
-		// 报错
 	} else {
-		
 	}
 }
- 
 
 function tagName(c) {
 	// <html name=1 属性
@@ -109,29 +112,29 @@ function tagName(c) {
 	} else if (c.match(/^[a-zA-Z]$/)) {
 		currentToken.tagName += c
 		return tagName
-	} else if (c === '>') { 
+	} else if (c === '>') {
 		emit(currentToken)
 		return data // 普通开始标签
 	} else {
+		currentToken.tagName += c
 		return tagName
 	}
 }
 
 /** 处理属性 <html lang="en"> */
 function beforeAttributeName(c) {
-		if (c.match(/^[\t\n\f ]$/)) {
-			return beforeAttributeName
-		} else if (c === '>'||c==='/'||c===EOF) {
-			return afterAttributeName(c)
-		} else if (c === '=') {
-		
-		} else {
-			currentAttribute = {
-				name: '',
-				value:''
-			}
-			return attributeName(c)
+	if (c.match(/^[\t\n\f ]$/)) {
+		return beforeAttributeName
+	} else if (c === '>' || c === '/' || c === EOF) {
+		return afterAttributeName(c)
+	} else if (c === '=') {
+	} else {
+		currentAttribute = {
+			name: '',
+			value: ''
 		}
+		return attributeName(c)
+	}
 }
 
 function attributeName(c) {
@@ -140,9 +143,7 @@ function attributeName(c) {
 	} else if (c === '=') {
 		return beforeAttributeValue
 	} else if (c === '\u0000') {
-		
-	} else if (c === "\"" || c === "'" || c === "<") {
-		
+	} else if (c === '"' || c === "'" || c === '<') {
 	} else {
 		currentAttribute.name += c
 		return attributeName
@@ -152,63 +153,42 @@ function attributeName(c) {
 function beforeAttributeValue(c) {
 	if (c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
 		return beforeAttributeValue
-	} else if (c === '\"') {
+	} else if (c === '"') {
 		return doubleQuotedAttributeValue
-	} else if (c === '\'') {
+	} else if (c === "'") {
 		return singleQuotedAttributeValue
 	} else if (c === '>') {
 		// return data
 	} else {
-				return UnQuotedAttributeValue(c)
+		return UnQuotedAttributeValue(c)
 	}
-
 }
 
 function doubleQuotedAttributeValue(c) {
-	if (c === "\"") {
+	if (c === '"') {
 		currentToken[currentAttribute.name] = currentAttribute.value
 		return afterQuotedAttributeValue
 	} else if (c === '\u0000') {
-		
 	} else if (c === EOF) {
-		
 	} else {
 		currentAttribute.value += c
 		return doubleQuotedAttributeValue
 	}
 }
 
-function afterQuotedAttributeValue(c) {
-	if (c.match(/^[\t\n\f ]$/)) {
-		return beforeAttributeName;
-	} else if (c === "/") {
-		return selfClosingStartTag;
-	} else if (c === ">") {
-		currentToken[currentAttribute.name] = currentAttribute.value;
-		emit(currentToken);
-		return data;
-	} else if (c === EOF) {
-	} else {
-		currentAttribute.value += c;
-		return doubleQuotedAttributeValue;
-	}
-}
 
 function singleQuotedAttributeValue(c) {
 	if (c === "'") {
 		currentToken[currentAttribute.name] = currentAttribute.value
 		return afterQuotedAttributeValue
 	} else if (c === '\u0000') {
-		
 	} else if (c === EOF) {
-		
 	} else {
 		currentAttribute.value += c
-		return doubleQuotedAttributeValue
+		return singleQuotedAttributeValue
 	}
 }
 
-// todo 3:49   37
 function UnQuotedAttributeValue(c) {
 	if (c.match(/^[\t\n\f ]$/)) {
 		currentToken[currentAttribute.name] = currentAttribute.value
@@ -217,67 +197,73 @@ function UnQuotedAttributeValue(c) {
 		currentToken[currentAttribute.name] = currentAttribute.value
 		return selfClosingStartTag
 	} else if (c === '>') {
-				currentToken[currentAttribute.name] = currentAttribute.value
+		currentToken[currentAttribute.name] = currentAttribute.value
 		emit(currentToken)
 		return data
 	} else if (c === '\u0000') {
-		
-	}else if(c==="\""||c==="'"||c==="<"||c==='='||c==="`"){
-
-	}else if(c===EOF){
-
-	}else{
+	} else if (c === '"' || c === "'" || c === '<' || c === '=' || c === '`') {
+	} else if (c === EOF) {
+	} else {
 		currentAttribute.value += c
 		return UnQuotedAttributeValue
 	}
 }
 
-
-function afterAttributeName(c) {
+function afterQuotedAttributeValue(c) {
 	if (c.match(/^[\t\n\f ]$/)) {
-		return afterAttributeName;
-	} else if (c === "/") {
-		return selfClosingStartTag;
-	} else if (c === "=") {
-		return beforeAttributeValue;
-	} else if (c === ">") {
-		currentToken[currentAttribute.name] = currentAttribute.value;
-		emit(currentToken);
-		return data;
+		return beforeAttributeName
+	} else if (c === '/') {
+		return selfClosingStartTag
+	} else if (c === '>') {
+		currentToken[currentAttribute.name] = currentAttribute.value
+		emit(currentToken)
+		return data
 	} else if (c === EOF) {
 	} else {
-		currentToken[currentAttribute.name] = currentToken.value;
-		currentAttribute = {
-			name: "",
-			value: "",
-		};
-		return attributeName(c);
+		currentAttribute.value += c
+		return doubleQuotedAttributeValue
 	}
 }
 
+function afterAttributeName(c) {
+	if (c.match(/^[\t\n\f ]$/)) {
+		return afterAttributeName
+	} else if (c === '/') {
+		return selfClosingStartTag
+	} else if (c === '=') {
+		return beforeAttributeValue
+	} else if (c === '>') {
+		currentToken[currentAttribute.name] = currentAttribute.value
+		emit(currentToken)
+		return data
+	} else if (c === EOF) {
+	} else {
+		currentToken[currentAttribute.name] = currentToken.value
+		currentAttribute = {
+			name: '',
+			value: ''
+		}
+		return attributeName(c)
+	}
+}
 
 function selfClosingStartTag(c) {
 	if (c === '>') {
 		currentToken.isSelfClosing = true
+		emit(currentToken)
 		return data
 	} else if (c === 'EOF') {
-		
 	} else {
-		
 	}
 }
 
-
-
-
-
-
-module.exports.parseHTML = function parseHTML(html){
-    let state =  data
-    for (let c of html) {
-        state = state(c)
-    }
-    state = state(EOF)
-    console.log('html',html)
-	console.log(stack[0])
+module.exports.parseHTML = function parseHTML(html) {
+	let state = data
+	for (let c of html) {
+		state = state(c)
+	}
+	state = state(EOF)
+	console.log('html', html)
+	console.log(stack)
+	return stack[0]
 }
--- Toy-Browser/http/server.js
+++ Toy-Browser/http/server.js
@@ -1,35 +1,52 @@
+
+
+
+
+
 const http = require('http')
 
-http.createServer((request,response)=>{
-    let body = []
-    request.on('error',err=>{
-        console.log(err)
-    }).on('data',chunk=>{
-        body.push(chunk.toString())
-    }).on('end',()=>{
-        // body = Buffer.concat(body).toString()
-        console.log('body',body)
-        response.writeHead(200,{'Content-Type':'text/html'})
-        // response.end('hello,world\n')
-			response.end(
-				`<html lang="en">
+http.createServer((req, res) => {
+	let body = []
+	req.on('error', err => {
+		console.error(err)
+	})
+		.on('data', chunk => {
+			console.log('chunk', chunk)
+			body.push(chunk)
+		})
+		.on('end', () => {
+			// body = Buffer.concat(body).toString();
+			console.log('body', body)
+			res.writeHead(200, { 'Content-Type': 'text/html' })
+			res.end(
+				`<html uname=sudongyu>
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+   <style>
+    #container {
+    width: 500px;
+    height: 300px;
+    display: flex;
+    background-color: rgb(255,255,255);
+}
+#container #myid{
+    width: 200px;
+    height: 100px;
+    background-color: rgb(255,0,0);
+}
+#container .c1{
+    flex: 1;
+    background-color: rgb(0,255,0);
+}
+   </style>
 </head>
-<body>
-	<div>
-		<img id="myId" />
-		<img />
-	</div>
+<body style="background: black">
+     <div id="container">
+        <div id="myid"></div>
+        <div class="c1"></div>
+    </div>
 </body>
-</html>
-			
-`
+</html>`
 			)
-    })
-}).listen(8088)
-
+		})
+}).listen('8088')
 console.log('server started on http://localhost:8088')
